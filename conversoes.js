8901ec4239e3d7a63ba4992313c416ac2fc8d1a6
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-04-23 14:16:38 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado 
+// booleanos 
+
+// conversão implícita
+const numero = 456;
+const numeroString = Number("456a");
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+// conversão explícita
---
+++ var-let-const.js
@@ -0,0 +1,34 @@
+//var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area;
+
+// let forma = 'retângulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retângulo') {
+//   area = altura * comprimento;
+// } else {
+//   area = (altura * comprimento) / 2;
+// }
+
+// console.log(area)
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+  area = altura * comprimento;
+} else {
+  area = (altura * comprimento) / 2;
+}
+
+console.log(area)
