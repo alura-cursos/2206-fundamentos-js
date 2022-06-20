a577d23e2f9118c5c4436b4247e5ab93f9106576
Juliana Amoasei <32266030+JulianaAmoasei@users.noreply.github.com>
2022-06-20 16:32:57 -0300

    update

--- truthy-falsy.js
+++ truthy-falsy.js
@@ -1,1 +1,36 @@
 const x = 0;
+
+//Boolean
+const usuarioLogado = true
+const contaPaga = false
+
+//truthy ou falsy
+//0 -> false
+//1 -> true
+
+console.log(0 == false)//retorna true
+console.log("" == false)//retorna true
+console.log(1 == true)
+
+
+console.log("null e undefinned")
+//undefined
+//null -> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log("Quando o undefined está em sua forma pura")
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
